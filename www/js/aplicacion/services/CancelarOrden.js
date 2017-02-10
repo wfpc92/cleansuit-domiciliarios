a72be0ffdc81f8a6a823c0747e0b78eb3e44a9a6
karenj2105 <karenj2105@gmail.com>
2017-02-10 00:20:43 -0500

    18. cuando se agregan las de 4 fotos, el cuadro de agregar se pierde, 2. ventana emergente suspender pedido informacion de orden de recoleccion,

--- www/js/aplicacion/services/CancelarOrden.js
+++ www/js/aplicacion/services/CancelarOrden.js
@@ -8,14 +8,14 @@ var CancelarOrdenFactory = function($ionicPopup,
 		$scope: null, 
 
 		textos: {
-			volverInfoOrden: "Volver a información de orden"
+			volverInfoOrden: "Regresar a información de orden"
 		},
 
 		motivos: [
-    		{ texto: "Valor elevado", valor: "0" },
-    		{ texto: "Manifiesta mala atención", valor: "1" },
-    		{ texto: "Prefiere otra empresa", valor: "2" }
-    	],
+			{ texto: "Valor elevado", valor: "0" },
+			{ texto: "Manifiesta mala atención", valor: "1" },
+			{ texto: "Prefiere otra empresa", valor: "2" }
+		],
 
 		cb: {
 			pendiente: function() {},
@@ -32,23 +32,28 @@ var CancelarOrdenFactory = function($ionicPopup,
 
 			$ionicPopup
 			.confirm({
-		    	title: 'Suspender pedido',
-		    	template: '',
-		    	buttons: [
-			    	{
-			    		text: 'Pedido pendiente',
-				    	type: 'button-calm',
-			    		onTap: self.cb.pendiente
-			    	},
-			      	{
-				    	text: '<b>Cancelar Pedido</b>',
-				    	type: 'button-calm',
-			    		onTap: function(e) {
+				title: 'Suspender pedido',
+				template: '',
+				buttons: [
+					{
+						text: 'Pedido pendiente',
+						type: 'button-calm ordPendiente',
+						onTap: self.cb.pendiente
+					},
+					{
+						text: 'Cancelar Pedido',
+						type: 'button-calm ordCancelada',
+						onTap: function(e) {
 							self.mostrarMotivos();
-			    		}
-			      	}
-			    ]
-		    });
+						}
+					}
+				]
+			}).then(function() {
+				var array = document.getElementsByClassName('popup-buttons');
+				for (var i = 0; i < array.length; i++) {
+					array[i].setAttribute('class', 'btnCulm');
+				};
+			})
 		},
 
 		mostrarMotivos: function() {
@@ -56,31 +61,31 @@ var CancelarOrdenFactory = function($ionicPopup,
 			
 			var scope = $rootScope.$new();
 			scope.motivos = self.motivos;
-	    	scope.motivo = {
+			scope.motivo = {
 				valor: scope.motivos[0].valor
 			};
 
 			$ionicPopup
 			.confirm({
-		    	title: 'Causa por la cual el cliente cancela el pedido',
-		    	templateUrl: "templates/app/orden/motivos-cancelacion-orden.html",
-		    	scope: scope,
-		    	buttons: [
-			    	{
-			    		text: self.textos.volverInfoOrden,
-				    	type: 'button-ligth',
-			    		onTap: self.cb.volverInfoOrden
-			    	},
-			      	{
-				    	text: '<b>Enviar</b>',
-				    	type: 'button-calm',
-			    		onTap: function(e) {
-			    			//enviar motivo de suspension de Pedido
+				title: 'Causa por la cual el cliente cancela el pedido',
+				templateUrl: "templates/app/orden/motivos-cancelacion-orden.html",
+				scope: scope,
+				buttons: [					
+					{
+						text: '<b>Enviar</b>',
+						type: 'button-calm enviarMotivo',
+						onTap: function(e) {
+							//enviar motivo de suspension de Pedido
 							self.mostrarEnviarCancelacion(scope.motivo.valor);
-			    		}
-			      	}
-			    ]
-		    });
+						}
+					},
+					{
+						text: self.textos.volverInfoOrden,
+						type: 'button-ligth volverCancelar',
+						onTap: self.cb.volverInfoOrden
+					}
+				]
+			});
 		},
 
 		mostrarEnviarCancelacion: function(indexMotivo) {
@@ -98,18 +103,18 @@ var CancelarOrdenFactory = function($ionicPopup,
 
 				$ionicPopup
 				.confirm({
-			    	title: 'La causa por la cual el cliente cancela el pedido '+txt,
-			    	template: '',
-			    	buttons: [
-				    	{
-				    		text: 'Aceptar',
-					    	type: 'button-ligth',
-				    		onTap: self.cb.cancelar 
-				    	},
-				    ]
-			    });
+					title: 'La causa por la cual el cliente cancela el pedido '+txt,
+					template: '',
+					buttons: [
+						{
+							text: 'Aceptar',
+							type: 'button-ligth',
+							onTap: self.cb.cancelar 
+						},
+					]
+				});
 
-			    self.cb.enviar();
+				self.cb.enviar();
 			});
 		},
 
@@ -118,23 +123,23 @@ var CancelarOrdenFactory = function($ionicPopup,
 
 			$ionicPopup
 			.confirm({
-		    	title: 'Validación del cliente',
-		    	template: '',
-		    	buttons: [
-			    	{
-			    		text: 'El cliente esta de acuerdo con la orden',
-				    	type: 'button-calm',
-			    		onTap: self.cb.deacuerdo
-			    	},
-			      	{
-				    	text: '<b>El cliente no esta de acuerdo con la orden</b>',
-				    	type: 'button-calm',
-			    		onTap: function(e) {
+				title: 'Validación del cliente',
+				template: '',
+				buttons: [
+					{
+						text: 'El cliente esta de acuerdo con la orden',
+						type: 'button-calm',
+						onTap: self.cb.deacuerdo
+					},
+					{
+						text: '<b>El cliente no esta de acuerdo con la orden</b>',
+						type: 'button-calm',
+						onTap: function(e) {
 							self.mostrarMotivos();
-			    		}
-			      	}
-			    ]
-		    });
+						}
+					}
+				]
+			});
 		}
 	};
 };
